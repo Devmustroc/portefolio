97197edf4b59a7beeaa25440416196d6e60c698a
Devmustroc <elmustapha.abourar@outlook.com>
2023-08-08 19:28:34 +0200

    :art: devider section components

---
+++ components/section-divider.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import React from 'react'
+import { motion } from 'framer-motion'
+
+export default function SectionDivider() {
+    return(
+        <motion.div className="bg-gray-200 my-24 h-16 w-1 rounded-full hidden sm:block"
+        initial={{
+            y: 100,
+            opacity: 0,
+        }}
+        animate={{
+            opacity: 1,
+            y: 0,
+        }}
+        transition={{
+            delay: 0.5,
+        }}>
+
+        </motion.div>
+    )
+};
